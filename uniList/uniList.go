50af0d1b53ab64c69dc317ffc1c0f9a0e791fd24
プログラミング <programing@fujitanaohironoMacBook-Air.local>
2021-07-04 12:54:36 +0900

    単方向連結リスト

---
+++ uniList/uniList.go
@@ -0,0 +1,67 @@
+package uniList
+
+import "fmt"
+
+// Element is structure.
+type Element struct {
+	Next *Element
+	Data int
+}
+
+// Node is method of Element.
+func (e *Element) Node(d int) {
+	e.Next = nil
+	e.Data = d
+}
+
+// AppendToTail is method of Element.
+func (e *Element) AppendToTail(d int) {
+	end := new(Element)
+	end.Node(d)
+	for e.Next != nil {
+		e = e.Next
+	}
+	e.Next = end
+}
+
+// AppendToTailList is method of Element
+func (e *Element) AppendToTailList(l *Element) {
+	for e.Next != nil {
+		e = e.Next
+	}
+	e.Next = l
+}
+
+// DisplayList is method.
+func (e *Element) DisplayList() {
+	for e != nil {
+		fmt.Print(e, " ")
+		e = e.Next
+	}
+	fmt.Println()
+}
+
+// DisplayListData is method.
+func (e *Element) DisplayListData() {
+	for e != nil {
+		fmt.Print(e.Data, " ")
+		e = e.Next
+	}
+	fmt.Println()
+}
+
+// DisplayList10Datas is method.
+func (e *Element) DisplayList10Datas() {
+	for i := 0; i < 10 && e != nil; i++ {
+		fmt.Print(e, " ")
+		e = e.Next
+	}
+	fmt.Println()
+}
+
+/*func main() {
+	e := new(Element)
+	e.Node(1)
+	e.AppendToTail(2)
+	displayList(e)
+}*/
